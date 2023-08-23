68b8c9dbea72ebdceaa6a1df6f69ce7805646e23
Jando <alexandroguterez@gmail.com>
2023-08-23 13:03:41 -0700

    add a minimal about page.

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Messages from './Messages'
+import About from './About'
 
 export default function Login(props) {
     if(props.friend) {
         return <Messages url={props.url} user={props.user} friend={props.friend} socket={props.socket}/>
     }
-    return <div>Hello welcome to my app</div>
+    return <About />
 }
