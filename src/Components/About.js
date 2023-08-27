3a3572251b45443a0653b993da6b374ffea31316
Jando <alexandroguterez@gmail.com>
2023-08-27 16:56:04 -0700

    remove aboutme page. it wasn't good

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,20 +4,7 @@ import "../Stylesheets/About.css";
 export default function Login() {
     return(
         <div>
-            <section className="about-section">
-    <div className="about-content">
-      <h1>What is this?</h1>
-      <p>This is a messaging app I made for my portfolio which will hopefully help me get a job.</p>
-      <p>Technologies utilized are React for the frontend; Express, Socket.io, Passport.js and MongoDB for the backend.</p>
-    </div>
-    <div className="about-image">
-      <img src="https://cdn.discordapp.com/attachments/765107990256287782/1143832424338444319/20210513_072317.jpg" alt="Alejandro Gutierrez Selfie" />
-    </div>
-      <ul>
-        <li><a href="mailto:alexandroguterez@gmail.com">alexandroguterez@gmail.com</a></li>
-        <li><a href="https://github.com/Jando-G" target="_blank" rel="noreferrer">Github</a></li>
-      </ul>
-  </section>
+           <div>Add a friend and send them a message!</div>
 
   <footer>
     <p>&copy; 2023 Alejandro Gutierrez</p>
